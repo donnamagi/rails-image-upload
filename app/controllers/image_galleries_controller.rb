404d86183b94fc229fba957ff4790ea146518ec5
Donna Mägi <magi.donna@gmail.com>
2023-11-03 23:55:50 +0100

    logic for handling multiple image uploads

--- app/controllers/image_galleries_controller.rb
+++ app/controllers/image_galleries_controller.rb
@@ -10,6 +10,10 @@ class ImageGalleriesController < ApplicationController
   end
 
   def create
+    # if multiple images are uploaded at once, the pictures will be nested in an array
+    # "images_attributes" => { "0" => { "picture" => [image, image2, image3] ...
+    # this will be rejected, so we need to restructure the params hash
+    handle_multiple_images if params[:image_gallery][:images_attributes]["0"][:picture].is_a?(Array)
     @image_gallery = ImageGallery.new(image_gallery_params)
 
     respond_to do |format|
@@ -22,6 +26,20 @@ class ImageGalleriesController < ApplicationController
   end
 
   private
+
+  def handle_multiple_images
+    # user might have already uploaded some images, so we get the last index
+    index = params[:image_gallery][:images_attributes].keys.last
+    pictures = params[:image_gallery][:images_attributes][index][:picture]
+    
+    restructured_pictures = 
+      pictures.each_with_index.map do |picture, index|
+        [index.to_s, { picture: picture }]
+      end
+    
+    # "images_attributes" => { "0" => { "picture": image }, "1" => { "picture": image2 } ...
+    params[:image_gallery][:images_attributes] = restructured_pictures.to_h
+  end
 
   def set_gallery
     @image_gallery = ImageGallery.find(params[:id])
